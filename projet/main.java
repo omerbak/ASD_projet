a19f8e12e29cb67ef37d561a931fe194adbb852c
Abrahim-BAMATRAF <ebrahem.f.b@gmail.com>
2020-11-30 20:07:35 +0100

    f you

--- projet/main.java
+++ projet/main.java
@@ -1,20 +1,25 @@
 import java.awt.Color;
 import java.io.PrintWriter;
 import java.util.*;
+import java.io.IOException;
 
 public class main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException  {
 		
-		try {
-            if (args.length!=1) throw new IllegalArgumentException("Preciser le nom (et chemin) d'un unique fichier png en paramatre");
+		////command line: java -jar imageCompress.jar <before.png> <numberFeuilles> <max degradation>
 
             ImagePNG png = new ImagePNG(args[0]);
             
             Quadtree tree = new Quadtree(png);
             
-            ImagePNG copy = new ImagePNG(png);
+            ImagePNG delta = new ImagePNG(png);
             
+            ImagePNG phi = new ImagePNG(png);
+            
+            
+            
+            /*
             //clearing the copy
             for (int x=0 ; x<copy.width(); x++)
             {
@@ -25,33 +30,24 @@ public class main {
 
                 }
             }
-            
-            
-            //******************testing elagage *********************
-            
-            /*
-            System.out.println(tree.toString());
-            
-            System.out.println("compress");
-            tree.elagage();
-            System.out.println(tree.toString());
-            
-            tree.toImage(copy,"elagage.png");
             */
+            
             
             
             
             //*****************testing compressDelta *****************
-            /*
+            
+            double degradation = Double.parseDouble(args[2]);
+            
             System.out.println(tree.toString());
             System.out.println("compress");
-            tree.compressDelta(70);
+            tree.compressDelta(degradation);
             System.out.println(tree.toString());
             
-            tree.toImage(copy,"delta.png");
+            tree.toImage(delta,"delta.png");
             
             System.out.println("done");
-            */
+            
             
             
             
@@ -61,7 +57,7 @@ public class main {
             //************************testing toImage**********************
             
             /*
-            tree.toImage(copy,"toImage.png");
+            tree.toImage(delta,"toImage.png");
             
             System.out.println("done");
             */
@@ -82,35 +78,16 @@ public class main {
             
             //******************testing compressPhi*********************
             
+            int nbFeuilles = Integer.parseInt(args[1]);
             
             System.out.println(tree.toString());
             System.out.println("compressing using compressPhi");
             
-            tree.compress_phi(2096);
-            
+            tree.compress_phi(nbFeuilles);
             
             System.out.println(tree.toString());
-            tree.toImage(copy,"phi.png");
-            try (PrintWriter out = new PrintWriter("toString.txt")) {
-                out.println(tree.toString());
-            }
-            
-            
-            
-          
-            
-            
-            
-            
-            
-
-            
-           
-            
-        
-		}catch (Exception e) {
-            e.printStackTrace();
-        }
+            tree.toImage(phi,"phi.png");
+      
 
 	}
 
